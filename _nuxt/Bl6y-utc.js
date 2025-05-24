c72519b66d94949c13a52f9b63b36a09307e9578
TsukiSama9292 <TsukiSama9292@users.noreply.github.com>
2025-05-24 00:09:09 +0000

    deploy: e18cc25b92f7c269c37121a98db2dbbd67a07014

---
+++ _nuxt/Bl6y-utc.js
@@ -0,0 +1,1 @@
+import{_ as o,c as s,o as r,M as t}from"./Be1Dt0ki.js";const n={};function c(e,a){return r(),s("tbody",null,[t(e.$slots,"default")])}const l=Object.assign(o(n,[["render",c]]),{__name:"ProseTbody"});export{l as default};
