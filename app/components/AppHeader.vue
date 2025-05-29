3bca77bcbb58a33d804e4b6c6b91da5df2aa8263
TsukiSama9292 <a0985821880@gmail.com>
2025-05-29 09:47:01 +0800

    [update] Header 修改顯示文字

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -26,15 +26,16 @@
       v-show="!drawer"
     >
       <v-btn
-        v-for="item in items"
-        :key="item"
-        class="mx-2"
+        v-for="[title, path] in Object.entries(items)"
+        :key="title"
+        @click="navigate(path)"
+        class="mx-2 font-weight-bold"
         color="white"
         variant="tonal"
         rounded="xl"
-        @click="navigate(item)"
+        size="large"
       >
-        {{ item }}
+        {{ title }}
       </v-btn>
     </div>
 
@@ -56,12 +57,12 @@
   >
     <v-list>
       <v-list-item
-        v-for="item in items"
-        :key="item"
-        @click="navigate(item)"
+        v-for="[title, path] in Object.entries(items)"
+        :key="title"
+        @click="navigate(path)"
       >
         <v-list-item-content>
-          <v-list-item-title>{{ item }}</v-list-item-title>
+          <v-list-item-title class="text-h5 py-4">{{ title }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -76,10 +77,16 @@ import { useRouter } from 'vue-router'
 const drawer = ref(false)
 const router = useRouter()
 
-const items = ref(['About', 'Blogs', 'Products', 'Supports'])
+const items = {
+  '關於': 'about',
+  '文章': 'blogs',
+  '產品': 'products',
+  '協作': 'supports'
+}
 
-function navigate(title: string) {
+// 點擊時使用路由 path
+function navigate(path: string) {
   drawer.value = false
-  router.push(`/${title.toLowerCase()}`)
+  router.push(`/${path}`)
 }
 </script>
