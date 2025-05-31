77f5fb2825bac09c8973057929f2db592d7169e2
TsukiSama9292 <a0985821880@gmail.com>
2025-05-31 11:04:23 +0800

    [update] 導航按鈕 hint

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -26,18 +26,22 @@
       style="position: absolute; left: 50%; transform: translateX(-50%);"
       v-show="!drawer"
     >
-      <v-btn
-        v-for="[title, path] in Object.entries(items)"
-        :key="title"
-        @click="navigate(path)"
-        class="mx-2 font-weight-bold"
-        color="white"
-        variant="tonal"
-        rounded="xl"
-        size="large"
-      >
-        {{ title }}
-      </v-btn>
+      <v-tooltip v-for="item in items" :text="item.hint" content-class="rounded-pill white-tooltip">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            v-bind="props"
+            :key="item.title"
+            @click="navigate(item.path)"
+            class="mx-2 font-weight-bold"
+            color="white"
+            variant="tonal"
+            rounded="xl"
+            size="large"
+          >
+            {{ item.title }}
+          </v-btn>
+        </template>
+      </v-tooltip>
     </div>
 
     <v-spacer />
@@ -58,12 +62,12 @@
   >
     <v-list>
       <v-list-item
-        v-for="[title, path] in Object.entries(items)"
-        :key="title"
-        @click="navigate(path)"
+        v-for="item in items"
+        :key="item.title"
+        @click="navigate(item.path)"
       >
         <v-list-item-content>
-          <v-list-item-title class="text-h5 py-4">{{ title }}</v-list-item-title>
+          <v-list-item-title class="text-h5 py-4">{{ item.title }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -78,13 +82,13 @@ import { useRouter } from 'vue-router'
 const drawer = ref(false)
 const router = useRouter()
 
-const items = {
-  '關於': 'about',
-  '文章': 'blogs',
-  '產品': 'products',
-  '協作': 'supports',
-  '聯絡': 'contact'
-}
+const items = [
+  { title: '關於', path: 'about', hint: '關於我們' },
+  { title: '文章', path: 'blogs', hint: '最新文章' },
+  { title: '產品', path: 'products', hint: '我們的產品' },
+  { title: '協作', path: 'supports', hint: '合作與贊助' },
+  { title: '聯絡', path: 'contact', hint: '聯絡我們' }
+]
 
 // 點擊時使用路由 path
 function navigate(path: string) {
@@ -92,3 +96,10 @@ function navigate(path: string) {
   router.push(`/${path}/`)
 }
 </script>
+
+<style scoped>
+.v-tooltip> ::v-deep(.v-overlay__content) {
+background: white;
+color: black;
+}
+</style>
